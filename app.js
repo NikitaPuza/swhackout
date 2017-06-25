3804621e0fe665b46908ac0c1f59b22cb17041ce
Kenny Loveall <kenny@kennyloveall.com>
2017-06-25 12:06:29 -0500

    Fixing bug.

--- app.js
+++ app.js
@@ -19,72 +19,65 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next){
   res.io = io;
   next();
 });
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var client = algoliasearch('Y8Q3VSESJ3', '2b9e09d99f2c106b00faed00c2664917');
 var index = client.initIndex('swhackout');
 
-const store = new RedisStore({url: '//redis-11046.c9.us-east-1-4.ec2.cloud.redislabs.com:11046'});
+const store = new RedisStore({url:'//redis-11046.c9.us-east-1-4.ec2.cloud.redislabs.com:11046'});
 
 app.use(helmet());
 app.use(session({
-  store: store,
-  secret: 'milhouse',
-  resave: true,
-  saveUninitialized: false,
-  name: 'fbauth',
-  ttl: 5000,
-  prefix: 'session',
-  cookie: {maxAge: 5000}
+	store: store,
+	secret: 'milhouse',
+	resave: true,
+	saveUninitialized: false,
+	name: 'fbauth',
+	ttl: 5000,
+	prefix:'session',
+	cookie: {maxAge: 5000}
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(new FacebookStrategy({
-      clientID: '663527113857308',
-      clientSecret: '2d2bf833552930100b344bad56ace8b0',
-      callbackURL: "https://swhackout.herokuapp.com/auth/facebook/callback"
+    clientID: '663527113857308',
+    clientSecret: '2d2bf833552930100b344bad56ace8b0',
+    callbackURL: "https://swhackout.herokuapp.com/auth/facebook/callback"
     },
-    (accessToken, refreshToken, profile, done) = > {
+  (accessToken, refreshToken, profile, done) => {
     index.addObject({
-    name: profile.displayName
-  }, profile.id, function (err, content) {
-    console.log('objectID=' + content.objectID);
-  });
-done(null, profile);
-}
-))
-;
+      name: profile.displayName
+    }, profile.id, function(err, content) {
+  console.log('objectID=' + content.objectID);
+});
+    	done(null, profile);
+		}
+));
 
-passport.serializeUser((user, done) = > {
-  done(null, user
-)
-;
-})
-;
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
 
-passport.deserializeUser((user, done) = > {
-  done(null, user
-)
-;
-})
-;
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
 
 
 function ensureAuthenticated(req, res, next) {
-  if (req.user != undefined) {
-    console.log(req.user);
-    return next();
-  }
-  res.redirect(302, '/login');
+	if (req.user != undefined) {
+		console.log(req.user);
+		return next();
+	}
+	res.redirect(302, '/login');
   console.log(req.passport)
 }
 
@@ -92,24 +85,21 @@ app.get('/auth/facebook',
   passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {failureRedirect: '/login', successRedirect: '/'}));
+  passport.authenticate('facebook', {failureRedirect: '/login', successRedirect: '/' }));
 
 
-app.get('/login', function (req, res) {
+app.get('/login', function(req, res) {
   res.render('login');
 });
 
 app.get('/', (req, res) => {
     res.redirect(302, '/events')
   //console.log(userProfile);
-  //parse = JSON.stringify(req.user._json);
-  res.render('index', {profile: 'parse'})
+  parse = JSON.stringify(req.user._json);
+  res.render('index', {profile: parse})
 })
 
 app.get('/events', function (req, res) {
-  /*if (req.user == undefined) {
-    res.redirect(302, '/login')
-  }
   var red_number = parseInt(req.user._json) % 256;
   var green_number = (parseInt(req.user._json) / 256) % 256;
   var blue_number = (parseInt(req.user._json) / 256 / 256) % 256;
@@ -118,26 +108,25 @@ app.get('/events', function (req, res) {
     'g': green_number,
     'b': blue_number
   };
-
   res.render('events', {profile: req.user._json, colors: colors});
-  // res.render('events');
 });
 app.get('/people', function (req, res) {
   res.render('people')
 })
 
-io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
 
 
 module.exports = {app: app, server: server};
